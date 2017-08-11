e54012d707537f564eea7592e34b82e39dd438bc
yangfc <uestc2803305011@gmaill.com>
2017-08-11 20:22:29 +0800

    initial commit

---
+++ hello.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+from flask import request
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+app = Flask(__name__)
+ 
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.route('/')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return render_template('index.html', current_time=datetime.utcnow())
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_erro(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    manager.run()
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
